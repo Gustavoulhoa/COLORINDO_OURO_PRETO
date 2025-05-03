4b4a06927f6db9fb456faba240edcb6dd1b39d48
Gustavoulhoa <gustavo.ulhoa@aluno.ufop.edu.br>
2025-05-02 23:16:49 -0300

    Delete js directory

--- js/gallery.js
+++
@@ -1,5 +0,0 @@
-// Caso precise de interações na galeria no futuro
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Galeria carregada');
-    // Adicione aqui futuras interações
-});
